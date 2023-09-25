da0a94dce05b536575a0d1dc7c8a8dd12adc319c
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2023-09-25 16:37:34 +0700

    nesting component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,35 +1,11 @@
 import React from "react";
-
+import UserInfo from "./UserInfo"
 class MyComponent extends React.Component {
-  state = {
-    name: "School",
-    address: "Phú Bình - Thái Nguyên",
-    age: 21,
-  };
-
-  handleOnChange(e) {
-    this.setState({
-      name: e.target.value
-    });
-  }
-
-  handleOnSubmit(e) {
-    e.preventDefault();
-    console.log(this.state)
-  }
-
   // JSX
   render() {
     return (
       <div>
-        <p>
-          My name is <b>{this.state.name}</b>, <b>{this.state.age}</b> years old and I'm from <b>{this.state.address}</b>
-        </p>
-
-        <form onSubmit={(e) => { this.handleOnSubmit(e) }}>
-          <input type="text" onChange={(e) => { this.handleOnChange(e) }} />
-          <input type="submit" value="Cập nhật" />
-        </form>
+        <UserInfo />
       </div>
     );
   }
