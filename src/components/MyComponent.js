6a348fa38bd814f28a52873513e9b56885531732
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2023-09-25 16:16:32 +0700

    form with react

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,13 +7,15 @@ class MyComponent extends React.Component {
     age: 21,
   };
 
-  handleClick(e) {
-    console.log("Hello mọi người, tui tên " + this.state.name);
+  handleOnChange(e) {
     this.setState({
-      name: "Xuân Trường",
-      address: "Thanh Ninh - Phú Bình - Thái Nguyên",
-      age: 22,
-    })
+      name: e.target.value
+    });
+  }
+
+  handleOnSubmit(e) {
+    e.preventDefault();
+    console.log(this.state)
   }
 
   // JSX
@@ -23,7 +25,11 @@ class MyComponent extends React.Component {
         <p>
           My name is <b>{this.state.name}</b>, <b>{this.state.age}</b> years old and I'm from <b>{this.state.address}</b>
         </p>
-        <button onClick={(e) => { this.handleClick(e) }}>Trò chuyện</button>
+
+        <form onSubmit={(e) => { this.handleOnSubmit(e) }}>
+          <input type="text" onChange={(e) => { this.handleOnChange(e) }} />
+          <input type="submit" value="Cập nhật" />
+        </form>
       </div>
     );
   }
