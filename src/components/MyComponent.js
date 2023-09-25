83198a8f1ecd7b3ff14881a74c0ccf472149bdfa
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2023-09-25 15:08:37 +0700

    state in react

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "School",
+    address: "Phú Bình - Thái Nguyên",
+    age: 21,
+  };
   // JSX
   render() {
-    return <div>My First Component</div>;
+    return (
+      <div>
+        My name is {this.state.name}, {this.state.age} years old and I'm from{" "}
+        {this.state.address}
+      </div>
+    );
   }
 }
 
