2e6b50f99ee561992ec9b905d662457ceadfc75a
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2023-09-25 15:46:30 +0700

    DOM event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,12 +6,21 @@ class MyComponent extends React.Component {
     address: "Phú Bình - Thái Nguyên",
     age: 21,
   };
+
+  handleClick(e) {
+    console.log("Hello mọi người");
+    console.log(e);
+  }
+
   // JSX
   render() {
     return (
       <div>
-        My name is {this.state.name}, {this.state.age} years old and I'm from{" "}
-        {this.state.address}
+        <p>
+          My name is {this.state.name}, {this.state.age} years old and I'm from{" "}
+          {this.state.address}
+        </p>
+        <button onClick={this.handleClick}>Trò chuyện</button>
       </div>
     );
   }
