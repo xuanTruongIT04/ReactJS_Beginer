0e5de1f9518889b466b5dc74822d0d4083a34bd7
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2023-09-25 14:27:14 +0700

    react component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+import logo from "./logo.svg";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
+import MyComponent from "./components/MyComponent";
 const App = () => {
-  const count = useSelector(state => state.counter.count);
+  const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world with Nguyễn Xuân Trường
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
+    <div>
+      <p>Hello world with Nguyễn Xuân Trường</p>
+      <MyComponent></MyComponent>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Hello world with Nguyễn Xuân Trường
+    //     </p>
+    //     <div>Count = {count}</div>
+    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+    //   </header>
+    // </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/MyComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  // JSX
+  render() {
+    return <div>My First Component</div>;
+  }
+}
+
+export default MyComponent;
