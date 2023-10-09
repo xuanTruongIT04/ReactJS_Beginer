13ccf67045bdbb15b44c5ab474e5ed54051d3feb
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2023-10-09 16:08:07 +0700

    index routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,19 +8,27 @@ const Header = () => {
   return (
     <Navbar expand="lg" className="bg-light">
       <Container>
-        <Navbar.Brand href="/">School</Navbar.Brand>
+        <NavLink to="/" className="navbar-brand">
+          School
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink to="/" className="nav-link">Home</NavLink>
-            <NavLink to="/users" className="nav-link">User</NavLink>
-            <NavLink to="/admins" className="nav-link">Admin</NavLink>
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+            <NavLink to="/users" className="nav-link">
+              User
+            </NavLink>
+            <NavLink to="/admins" className="nav-link">
+              Admin
+            </NavLink>
           </Nav>
           <Nav>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
-              <NavDropdown.Item href="#login">Login</NavDropdown.Item>
-              <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
+              <NavDropdown.Item>Profile</NavDropdown.Item>
+              <NavDropdown.Item>Login</NavDropdown.Item>
+              <NavDropdown.Item>Logout</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div className="homepage-container">HomePage component</div>;
+};
+
+export default HomePage;
